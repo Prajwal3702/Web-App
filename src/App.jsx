790c722d8f121ad15d1f050c073ab82c423b33e5
PrajwalK <prajwalkorade@gmail.com>
2025-03-20 11:49:30 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/events" element={<EventsPage />} />
-          {/* Redirect any unknown paths to home */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
@@ -25,24 +24,3 @@ function App() {
 }
 
 export default App;
-
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import HomePage from "./pages/HomePage/HomePage";
-// import EventsPage from "./pages/EventsPage/EventsPage";
-// import "./App.css";
-
-// function App() {
-//   return (
-//     <div className="app">
-//       <Router>
-//         <Routes>
-//           <Route path="/" element={<HomePage />} />
-//           <Route path="/events" element={<EventsPage />} />
-//         </Routes>
-//       </Router>
-//     </div>
-//   );
-// }
-
-// export default App;
